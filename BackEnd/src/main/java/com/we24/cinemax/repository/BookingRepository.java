c532c1be31a9c8544cfbb764b197413289810a4c
IT25100168@my.sliit.lk <IT25100168>
2026-05-20 19:23:23 +0530

    Repository part is completed

---
+++ BackEnd/src/main/java/com/we24/cinemax/repository/BookingRepository.java
@@ -0,0 +1,16 @@
+package com.we24.cinemax.repository;
+
+import com.we24.cinemax.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import java.util.Optional;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByUser_GmailOrderByCreatedAtDesc(String gmail);
+    Optional<Booking> findByBookingReference(String bookingReference);
+    boolean existsByScreenTime_Movie_Id(Long movieId);
+}
