60b1d791a49ebc33115e6da4d2ae20e64ce920fc
IT25100518 <it25100518@my.sliit.lk>
2026-05-21 16:24:04 +0530

    ScreenTime.java

---
+++ BackEnd/src/main/java/com/we24/cinemax/entity/ScreenTime.java
@@ -0,0 +1,38 @@
+package main.java.com.we24.cinemax.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "screen_times")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ScreenTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate showDate;
+
+    private LocalTime showTime;
+
+    private Integer screenNumber;
+
+    private Double ticketPrice;
+    
+    @Column(nullable = false)
+    @Builder.Default
+    private String status = "ACTIVE";
+
+    // Many screen times belong to one movie
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+}
