88e1ac3b0e3d987a49c53abc0e1bd5cf905b9e1b
IT25100518 <it25100518@my.sliit.lk>
2026-05-21 16:36:41 +0530

    ScreenTimeService.java

---
+++ BackEnd/src/main/java/com/we24/cinemax/service/ScreenTimeService.java
@@ -0,0 +1,23 @@
+package main.java.com.we24.cinemax.service;
+
+import com.we24.cinemax.model.ScreenTimeRequest;
+import com.we24.cinemax.model.ScreenTimeResponse;
+
+import java.util.List;
+
+public interface ScreenTimeService {
+
+    ScreenTimeResponse createScreenTime(ScreenTimeRequest request);
+
+    List<ScreenTimeResponse> getAllScreenTimes();
+
+    ScreenTimeResponse getScreenTimeById(Long id);
+
+    ScreenTimeResponse updateScreenTime(Long id, ScreenTimeRequest request);
+
+    void deleteScreenTime(Long id);
+
+    List<ScreenTimeResponse> getScreenTimesByMovie(Long movieId);
+
+    List<String> getReservedSeats(Long screenTimeId);
+}
